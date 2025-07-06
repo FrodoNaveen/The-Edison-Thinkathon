193f45e914767d856cf4b1fde758f612dc6ea86b
anglofone <anglofone@anglofones-MacBook-Air.local>
2025-07-06 13:38:29 +0530

    changed the button size

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -119,7 +119,7 @@ export default function LoginPage() {
 
                         }}
 
-                        width={320} // may work in some cases
+                        width={360} // may work in some cases
                     />
 
 
